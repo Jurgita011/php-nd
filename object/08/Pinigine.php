a862fd6f7d6b515e7e7fe383b5820a1be21bc0ab
Jurgita <jurgita011@gmail.com>
2023-06-13 14:19:20 +0300

    namu darbai

---
+++ object/08/Pinigine.php
@@ -0,0 +1,25 @@
+<?php
+class Pinigine{
+ private $popieriniaiPinigai;
+    private $metaliniaiPinigai;
+    
+    public function ideti($kiekis) :void
+    {
+        if ($kiekis <= 2) {
+            $this->metaliniaiPinigai += $kiekis;
+        } else {
+            $this->popieriniaiPinigai += $kiekis;
+        }
+
+}
+
+public function skaiciuoti ():void
+{
+ echo '<h1>Popieriniai pinigai: ' . $this->popieriniaiPinigai . '<br>Metaliniai pinigai: ' . $this->metaliniaiPinigai . '</h1>';
+}
+
+
+}
+
+
+?>
---
+++ object/08/index.php
@@ -0,0 +1,17 @@
+<?php
+echo '<pre>';
+
+require __DIR__ . '/Pinigine.php';
+
+$pinigine = new Pinigine();
+
+// Pridedame pinigus
+$pinigine->ideti(1); // Pridedame 1 į metalinius pinigus
+$pinigine->ideti(5); // Pridedame 5 į popierinius pinigus
+$pinigine->ideti(3); // Pridedame 3 į popierinius pinigus
+
+// Skaičiuojame ir atspausdiname sumą
+$pinigine->skaiciuoti();
+
+
+?>
